05ad6fd54cb7c33d559d4451184fbf33aeffb03b
Manohar Verma <indukuri.k@northeastern.edu>
2022-12-06 16:56:13 -0500

    chore(EmployeeDetails): Updated Entity

--- src/main/java/model/Employees/EmployeeDetails.java
+++ src/main/java/model/Employees/EmployeeDetails.java
@@ -4,7 +4,9 @@
  */
 package model.Employees;
 
+import java.io.Serializable;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,7 +15,9 @@ import javax.persistence.Id;
  *
  * @author manohar
  */
-public class EmployeeDetails {
+
+@Entity
+public class EmployeeDetails implements Serializable {
     
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
